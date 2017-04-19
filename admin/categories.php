0ebc8bf177de5d2986d4c51624b4d7c21e3c855d
Djordje Markovic <djordjem.markovic1@gmail.com>
2017-04-19 23:56:41 +0200

    Admin Area

---
+++ admin/categories.php
@@ -0,0 +1,68 @@
+<?php include "includes/admin_header.php"; ?>
+
+<body>
+    <div id="wrapper">
+        
+        <!-- Navigation -->
+        <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>
+                        <div class="col-xs-6"><!-- The Add Category Form -->
+                        
+                        <?php InsertCategories(); ?>               
+                        
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="cat_title">Add Category</label>
+                                <input type="text" class="form-control" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                            </div>
+                        </form>
+                            
+                        <?php // UPDATE AND INCLUDE QUERY
+                        if(isset($_GET['edit'])){
+                            $cat_id = $_GET['edit'];
+                            include "includes/update_categories.php";
+                        }
+                        ?>
+                        
+                        </div><!-- The Add Category Form End -->
+                        
+                        <div class="col-xs-6"><!-- The Categories Table --> <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Category Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                   
+                            <?php FindAllCategories(); ?>
+                            <?php DeleteCategories(); ?>
+
+                            </tbody>
+                        </table>    
+                        </div><!-- The Categories Table End --> 
+                                 
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php"; ?>

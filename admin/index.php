745b2950ad5a325d5cde34c131668a3382bfafdc
abdallah <abdallah.salah9700@gmail.com>
2019-07-19 17:40:11 +0200

    i romove some

--- admin/index.php
+++ admin/index.php
@@ -14,7 +14,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Welcome to Admin
+                            
                             <small>Author</small>
                         </h1>
                     </div>
